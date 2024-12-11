2d734ef87f01f6faf17e20ed2e201238e9a9b18a
vanphan24 <89482663+vanphan24@users.noreply.github.com>
2024-12-11 15:20:00 -0800

    Update variable.tf

--- Azure/variable.tf
+++ Azure/variable.tf
@@ -10,19 +10,19 @@ variable "azure_subscription" {
 variable "boundary_addr" {
   description = "boundary cluster address"
   type        = string
-  default     = "https://7a677e2c-ed79-4ff1-b760-708c9b5901e9.boundary.hashicorp.cloud/" # CHANGE VALUE TO MATCH YOUR CLUSTER
+  default     = "https://7a677e2c-ed79-4ff1-b760-12345678.boundary.hashicorp.cloud/" # CHANGE VALUE TO MATCH YOUR CLUSTER
 }
 
 variable "boundary_scope_id" {
   description = "Project scope for new host catalog"
   type        = string
-  default     = "p_mbpTEENkKs" # CHANGE VALUE TO MATCH YOUR SCOPE
+  default     = "p_mbp123456" # CHANGE VALUE TO MATCH YOUR SCOPE
 }
 
 variable "boundary_auth_method_id" {
   description = "boundary auth method ID"
   type        = string
-  default     = "ampw_SBZ0hG36lW" # CHANGE VALUE TO MATCH YOUR AUTH METHOD ID
+  default     = "ampw_SBZ123456" # CHANGE VALUE TO MATCH YOUR AUTH METHOD ID
   sensitive = true
 }
 
@@ -30,20 +30,13 @@ variable "boundary_auth_method_id" {
 variable "boundary_login_name" {
   description = "boundary username"
   type        = string
-  default     = "admin" # CHANGE VALUE TO MATCH YOUR LOGIN
+  default     = "my-admin-username" # CHANGE VALUE TO MATCH YOUR LOGIN
   sensitive = true
 }
 
 variable "boundary_password" {
   description = "boundary password"
   type        = string
-  default     = "Osmium76!cbs" # CHANGE VALUE TO MATCH YOUR PASSWORD. OR ENTER PASSWORD ON COMMAND LINE WITH -var FLAG
-  sensitive = true
-}
-
-variable "boundary_password1" {
-  description = "boundary password"
-  type        = string
-  default     = "Osmium76!cbs" # CHANGE VALUE TO MATCH YOUR PASSWORD. OR ENTER PASSWORD ON COMMAND LINE WITH -var FLAG
+  default     = "my-password-1234" # CHANGE VALUE TO MATCH YOUR PASSWORD. OR ENTER PASSWORD ON COMMAND LINE WITH -var FLAG
   sensitive = true
 }
